ffaf4391f5ae78454d199da4e77249eb70c5f844
Niwhar Amin <145506476+ninoa2000@users.noreply.github.com>
2025-03-09 14:56:30 +0000

    Add files via upload

---
+++ Category.js
@@ -0,0 +1,17 @@
+class Category {
+    // Attributes
+    category_ID;
+    name;
+
+    // Constructor
+    constructor() {}
+
+    // Methods
+    async method1() {}
+    async method2() {}
+}
+
+// Export the interface-like class
+module.exports = {
+    Category
+};
