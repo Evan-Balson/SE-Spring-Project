ccd849089ccbee023c93aadedd8108ae42707142
m12rezaei <m12rezaei@yahoo.com>
2025-03-12 21:08:10 +0000

    Add category class

--- Scaffolding-Files/app/models/category.js
+++ Scaffolding-Files/app/models/category.js
@@ -16,7 +16,21 @@ class category {
 
     // Methods
     
-    async getCategory_Name(){}
+    async getCategory_Name(){
+        try {
+            const [rows] = await db.query('SELECT category_name FROM Category WHERE category_name =?',
+                [this.category_Name]
+            );
+            if (rows.length > 0) {
+                return rows[0].category_Name;
+            }else {
+                return null; // Category not found
+            }
+        } catch (error) {
+            console.error('Error getting category name:', error);
+            return null;
+        }
+    }
 
 }
 
