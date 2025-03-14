4ebb0d6a9ec1294f688950dad6c89b505b349606
Niwhar Amin <aminniwhar0201@gmail.com>
2025-03-14 03:58:03 +0000

    last push

--- Scaffolding-Files/app/models/Inventory.js
+++ Scaffolding-Files/app/models/Inventory.js
@@ -40,3 +40,44 @@ class Inventory {
             return false; // Item not available
         }
     }
+    async return(){
+        this.quantity++;
+        this.isAvailable = true;
+        try{
+            await db.query('UPDATE Inventory SET quantity = ? , item_availability = ? WHERE inventory_id =?',
+                [this.quantity, this.isAvailable, this.Inventory_ID]);
+                return true; // Return successful
+        } catch (error) {
+            console.error('Error returning item: ', error);
+            return false; // Return failed
+        }
+    }
+    async displayinventory(){
+        try {
+            const [rows] = await db.query ('SELECT * FROM Inventory WHERE inventory_id=?', [this.Inventory_ID]);
+            if (rows.length > 0) {
+                const inventoryData = rows[0];
+                console.log('Inventory Details:');
+                console.log('Inventory ID: ${inventoryData.inventory_id}');
+                console.log('Price: ${inventoryData.price}');
+                console.log('Availability: ${inventoryData.item_availability}');
+                console.log('Quantity: ${inventoryData.quantity}');
+
+                const outfit = new Outfit(this.outfit.outfit_id, this.outfit.outfit_name, this.outfit.outfit_color, this.outfit.outfit_size, this.outfit.outfit_description, this.outfit.item_condition, this.outfit.user_id);
+                await outfit.displayOutfit();
+                return inventoryData;
+            } else {
+                console.log('Inventory not found.');
+                return null;
+            }        
+        } catch (error) {
+            console.error('Error displaying inventory: ', error);
+            return null;
+        }
+    }
+}
+
+// needed to  export functions, objects, or other values from a module so they can be imported and used in other files.
+module.exports = {
+    Inventory
+}
