e49fb4bbeb1a08b2722057aeca14fd757a7f3e96
Niwhar Amin <aminniwhar0201@gmail.com>
2025-03-14 03:50:33 +0000

    change to inventory

--- Scaffolding-Files/app/controllers/inventoryController.js
+++ Scaffolding-Files/app/controllers/inventoryController.js
@@ -20,3 +20,44 @@ async function rentItem(req, res) {
         res.status(500).json({message: 'Internal server error'});
     }
 }
+async function returnItem(req, res) {
+    try {
+        const inventoryId = req.params.id;
+        const inventory = await Inventory.getInventoryById(inventoryId);
+
+        if (!inventory){
+            return res.status(404).json({message: 'Inventory item not found'});
+        }
+        const returned = await inventory.return();
+
+        if (returned) {
+            res.status(200).json({message: 'Item returned successfully'});
+        } else {
+            res.status(500).json({message: 'Return failed'});
+        }
+    } catch (error) {
+        console.error('Error returning item:', error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+async function displayInventory(req, res){
+    try{
+        const inventoryId = req.params.id;
+        const inventory =await Inventory.getInventoryByID(inventoryId);
+
+        if (!inventory) {
+            return res.status(404).json({message: 'Inventory item not found'});
+        }
+        const inventoryDetails = await inventory.displayInventory();
+
+        if (inventoryDetails) {
+            res.status(200).json(inventoryDetails);
+        } else {
+            res.status(500).json({message: 'Failed to display inventory details'});
+        }
+    } catch (error) {
+        console.error('Error displaying inventory: ', error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
