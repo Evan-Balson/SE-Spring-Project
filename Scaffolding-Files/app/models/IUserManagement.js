9f323d53bd09af8bf029e8909d5140f1c22b2061
Hala Bakhtiar <halabakhtiar@gmail.com>
2025-03-15 22:11:13 +0000

    uploading interface for user management

---
+++ Scaffolding-Files/app/models/IUserManagement.js
@@ -0,0 +1,42 @@
+class IUserManagement{
+
+    buy(){
+        throw new Error('Method "buy" must be implemented');
+    }
+    sell(){
+        throw new Error('Method "sell" must be implemented');
+    }
+    addOutfit(){
+        throw new Error('Method "addOutfit" must be implemented');
+    }
+    writeReview(){
+        throw new Error('Method "writeReview" must be implemented');
+    }
+    
+    // there will be a payment method as a mitigation to miscuonduct and damages.
+    addPaymentMethod(){
+        throw new Error('Method "writeReview" must be implemented');
+    }
+
+    updatePaymentMethod(){
+        throw new Error('Method "writeReview" must be implemented');
+    }
+
+    // note a payment method whould only be removed if the use is updating
+
+    removePaymentMethod(){
+        throw new Error('Method "writeReview" must be implemented');
+    }
+
+    //User Must also login & Logout
+    Login(){}
+
+    Logout(){}
+
+
+}
+//export default IUserManagement;
+
+module.exports = {
+    IUserManagement
+}
