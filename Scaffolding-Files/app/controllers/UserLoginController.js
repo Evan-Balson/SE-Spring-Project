754802ff3a07020fff04a12ac1e1b59fe2f4d39a
Hala Bakhtiar <halabakhtiar@gmail.com>
2025-03-17 21:36:19 +0000

    userlogincontroller

--- Scaffolding-Files/app/controllers/UserLoginController.js
+++ Scaffolding-Files/app/controllers/UserLoginController.js
@@ -1,5 +1,4 @@
-// controllers/userController.js
-const User = require('../models/User');
+const { User } = require("../models/User");
 
 exports.login = async (req, res) => {
 try {
@@ -8,7 +7,8 @@ const user = await User.authenticate(email, password);
 if (user) {
 res.render('home-logged-in', { user: user });
 } else {
-res.render('login', { error: 'Invalid email or password' });
+            // Invalid login
+            res.render("login", { title: 'Login', error: 'Invalid email or password' });
 }
 } catch (error) {
 res.status(500).send(error.toString());
--- Scaffolding-Files/app/controllers/UserloginController.js
+++ Scaffolding-Files/app/controllers/UserloginController.js
@@ -3,26 +3,14 @@ const { User } = require("../models/User");
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        
-        // Authenticate user with the provided email and password
-        const user = await User.login(email, password);
-        
+const user = await User.authenticate(email, password);
         if (user) {
-            // Store user info in session
-            user.setLoginStatus(true);
-            req.session.activeUser = user;
-            
-
-            // Redirect to home page (root)
-            return res.redirect("/");
+res.render('home-logged-in', { user: user });
         } else {
             // Invalid login
             res.render("login", { title: 'Login', error: 'Invalid email or password' });
         }
     } catch (error) {
-        // Log and send error message
-        console.error('Error during login:', error);
-        res.status(500).send('Internal Server Error');
+res.status(500).send(error.toString());
     }
 };
-
