5f9e5500c0feda366a72927205808701b82ef4ff
Hala Bakhtiar <halabakhtiar@gmail.com>
2025-04-04 14:34:52 +0100

    updating the workflow test files

---
+++ Scaffolding-Files/tests/UserLoginController.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('UserLoginController', () => {
+  it('should return true for a basic test', () => {
+    expect(true).to.be.true;
+  });
+
+  // Add more test cases here
+});
